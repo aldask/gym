b8b441d45489e1b52f57a75c0554378c57b77240
Aldas <80693733+aldask@users.noreply.github.com>
2023-06-04 17:11:32 +0300

    Added new files

---
+++ src/Components/WhyUs/WhyUs.tsx
@@ -0,0 +1,12 @@
+function WhyUs () {
+    return (
+        <section className="why-us">
+            <div className="container">
+                <div className="why-us__box">
+                    new box
+                </div>
+            </div>
+        </section>
+    )
+}
+export default WhyUs;
--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import Hero from "../Components/Hero/Hero";
+import WhyUs from "../Components/WhyUs/WhyUs";
 
 function HomePage() {
   return (
     <>
       <Hero />
+      <WhyUs />
     </>
   );
 }
