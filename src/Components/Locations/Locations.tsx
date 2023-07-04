02c0f2bff505ecf939a1f9d09e0812e7dc488a2a
Aldas <80693733+aldask@users.noreply.github.com>
2023-07-04 20:14:59 +0300

    Styled side1

---
+++ src/Components/Locations/Locations.tsx
@@ -0,0 +1,29 @@
+function Locations() {
+  return (
+    <section className="locations">
+      <div className="container">
+        <div className="location-box">
+          <div className="location-box__side1">
+            <h2 className="location-box__side1--title">
+              FIND LEMON GYM CLUB THAT SUITS YOU BEST
+            </h2>
+            <p className="location-box__side1--text">
+              9 locations in Lithuania. Lemon Gym clubs are situated in
+              easy-to-find public places with 24/7 access.
+            </p>
+            <div className="location-box__side1--buttons">
+              <button className="location-box__side1--buttons--explore">
+                Explore
+              </button>
+              <button className="location-box__side1--buttons--join">
+                Join Us
+              </button>
+            </div>
+          </div>
+          <div className="location-box__side2">gymo lokacijos boxas</div>
+        </div>
+      </div>
+    </section>
+  );
+}
+export default Locations;
