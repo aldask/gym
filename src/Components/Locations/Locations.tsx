34f662ad3dc8dd7e349abd2c0236bd6a6b1bc5d6
Aldas <80693733+aldask@users.noreply.github.com>
2023-07-06 21:37:38 +0300

    Slieds kind of works. Needs to finis styling

--- src/Components/Locations/Locations.tsx
+++ src/Components/Locations/Locations.tsx
@@ -1,6 +1,46 @@
+import { useState } from "react";
 import pilaite from "../../Images/Locations/pilaite.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleArrowLeft,
+  faCircleArrowRight,
+} from "@fortawesome/free-solid-svg-icons";
+import Slider from "../Slider";
 
 function Locations() {
+  const sliders = [
+    {
+      locationPic: pilaite,
+      gymName: "Pilaite",
+      gymStreet: "axcvxcvxcvs",
+      gymPerks: "perks here",
+    },
+    {
+      locationPic: pilaite,
+      gymName: "tessss",
+      gymStreet: "asdasdasd",
+      gymPerks: "perks here",
+    },
+    {
+      locationPic: pilaite,
+      gymName: "Pdasdae",
+      gymStreet: "aadsasd",
+      gymPerks: "perks here",
+    },
+  ];
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % sliders.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide(
+      (prevSlide) => (prevSlide - 1 + sliders.length) % sliders.length
+    );
+  };
+
   return (
     <section className="locations">
       <div className="container">
@@ -10,7 +50,7 @@ function Locations() {
               FIND NEAREST GYM THAT SUITS YOU BEST
             </h2>
             <p className="locations__box__side1__text">
-              A variety locations in Lithuania. Gym clubs are situated in
+              A variety of locations in Lithuania. Gym clubs are situated in
               easy-to-find public places with 24/7 access.
             </p>
             <div className="locations__box__side1__buttons">
@@ -23,23 +63,31 @@ function Locations() {
             </div>
           </div>
           <div className="locations__box__side2">
-            <div className="locations__box__side2__gym-box">
-              <div className="locations__box__side2__gym-box__img">
-                <img src={pilaite} alt="gym" />
-              </div>
-              <div className="locations__box__side2__gym-box__content">
-                <div className="locations__box__side2__gym-box__content--name">
-                  <h1>Pilaite</h1>
-                  <p>Pilaitė, Vyduno g. 2</p>
-                </div>
-                <div className="locations__box__side2__gym-box__content--perks">
-                  gym perks here (icons and etc)
-                </div>
-                <div className="locations__box__side2__gym-box__content--about">
-                  <button>Visi Club Page</button>
-                </div>
-              </div>
+            <FontAwesomeIcon
+              icon={faCircleArrowLeft}
+              className="arrow"
+              onClick={prevSlide}
+            />
+            <div
+              className="locations__box__side2__slider-wrapper"
+              style={{ transform: `translateX(-${450 * currentSlide}px)` }}
+            >
+              {sliders.map((slider, index) => (
+                <Slider
+                  key={index}
+                  locationPic={slider.locationPic}
+                  gymName={slider.gymName}
+                  gymStreet={slider.gymStreet}
+                  gymPerks={slider.gymPerks}
+                  active={index === currentSlide}
+                />
+              ))}
             </div>
+            <FontAwesomeIcon
+              icon={faCircleArrowRight}
+              className="arrow"
+              onClick={nextSlide}
+            />
           </div>
         </div>
       </div>
