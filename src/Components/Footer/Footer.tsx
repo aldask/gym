fc2b00d16a183f42c71be2e3fb4c14f4802b7b7a
Aldas <80693733+aldask@users.noreply.github.com>
2023-07-12 22:34:12 +0300

    Footer layout done

---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,21 @@
+function Footer() {
+  return (
+    <section className="footer">
+      <div className="container">
+        <div className="footer-box">
+          <div className="footer-box__top">
+            <div className="footer-box__top--1row">logos here</div>
+            <div className="footer-box__top--1col">trainings</div>
+            <div className="footer-box__top--1col">additional info</div>
+            <div className="footer-box__top--1col">company</div>
+            <div className="footer-box__top--1col">connect</div>
+          </div>
+          <div className="footer-box__bottom">
+            <div className="footer-box__bottom-1row">terms and policy</div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+export default Footer;
--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -3,6 +3,7 @@ import Hero from "../Components/Hero/Hero";
 import Locations from "../Components/Locations/Locations";
 import Subscription from "../Components/Subscription/Subscription";
 import WhyUs from "../Components/WhyUs/WhyUs";
+import Footer from "../Components/Footer/Footer";
 
 function HomePage() {
   return (
@@ -12,6 +13,7 @@ function HomePage() {
       <Subscription />
       <GroupTrainings />
       <Locations />
+      <Footer />
     </>
   );
 }
