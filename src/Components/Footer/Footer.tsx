64ef677b9bd9b270b802ddb0b77942f506026204
Aldas <80693733+aldask@users.noreply.github.com>
2023-07-18 21:17:54 +0300

    Layout kind of done

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -4,11 +4,35 @@ function Footer() {
       <div className="container">
         <div className="footer-box">
           <div className="footer-box__top">
-            <div className="footer-box__top--1row">logos here</div>
-            <div className="footer-box__top--1col">trainings</div>
-            <div className="footer-box__top--1col">additional info</div>
-            <div className="footer-box__top--1col">company</div>
-            <div className="footer-box__top--1col">connect</div>
+            <div className="footer-box__top--1row">
+              <p>logo0</p>
+              <p>logo1</p>
+              <p>logo2</p>
+            </div>
+            <div className="footer-box__top--1col">
+              <p>items</p>
+              <p>items</p>
+              <p>items</p>
+              <p>items</p>
+            </div>
+            <div className="footer-box__top--1col">
+              <p>items</p>
+              <p>items</p>
+              <p>items</p>
+              <p>items</p>
+            </div>
+            <div className="footer-box__top--1col">
+              <p>items</p>
+              <p>items</p>
+              <p>items</p>
+              <p>items</p>
+            </div>
+            <div className="footer-box__top--1col">
+              <p>items</p>
+              <p>items</p>
+              <p>items</p>
+              <p>items</p>
+            </div>
           </div>
           <div className="footer-box__bottom">
             <div className="footer-box__bottom-1row">terms and policy</div>
