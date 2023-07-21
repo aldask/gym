8c7ce74a9a1ee823d83a2d1b0cbd23fbe4c70002
Aldas <80693733+aldask@users.noreply.github.com>
2023-07-21 21:08:53 +0300

    Need to save now

--- src/Components/Maps/Maps.tsx
+++ src/Components/Maps/Maps.tsx
@@ -1,8 +1,10 @@
-function Maps (){
-    return (
-        <section className="maps">
-            map here
-        </section>
-    )
-};
+import { MapContainer } from "react-leaflet";
+
+function Maps() {
+  return (
+    <section className="maps">
+      <MapContainer center={[48.8566, 2.3522]} zoom={13}></MapContainer>
+    </section>
+  );
+}
 export default Maps;
