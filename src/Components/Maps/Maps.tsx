c6d06847c21d8bbcaa78ce1279a42cbb0435c46d
Aldas <80693733+aldask@users.noreply.github.com>
2023-07-22 11:20:09 +0300

    Leaflet installed and set

--- src/Components/Maps/Maps.tsx
+++ src/Components/Maps/Maps.tsx
@@ -1,8 +1,15 @@
-function Maps (){
-    return (
-        <section className="maps">
-            map here
-        </section>
-    )
-};
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+function Maps() {
+  return (
+    <section className="maps">
+      <MapContainer center={[54.687157, 25.279652]} zoom={13}>
+        <TileLayer
+          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </section>
+  );
+}
 export default Maps;
