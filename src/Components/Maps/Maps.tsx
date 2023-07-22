e672dab0ecfb174f5557d1f4aa4bb194eaad656a
Aldas <80693733+aldask@users.noreply.github.com>
2023-07-22 11:53:45 +0300

    Added pins, added pop ups. Now need to give normal data

--- src/Components/Maps/Maps.tsx
+++ src/Components/Maps/Maps.tsx
@@ -1,15 +1,30 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import { slides } from "../../Data/locationsData";
+import { Icon } from "leaflet";
 
 function Maps() {
+  const customIcon = new Icon({
+    iconUrl:
+      "https://static.vecteezy.com/system/resources/previews/009/589/758/original/location-location-pin-location-icon-transparent-free-png.png",
+    iconSize: [50, 50],
+  });
   return (
     <section className="maps">
       <MapContainer center={[54.687157, 25.279652]} zoom={13}>
-        <TileLayer
-          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+        <TileLayer url="https://tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        {slides.map((marker) => (
+          <Marker
+            position={marker.gymCord as L.LatLngTuple}
+            key={marker.gymName}
+            icon={customIcon}
+          >
+            <Popup>{marker.popUp}</Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </section>
   );
 }
+
 export default Maps;
--- src/Data/locationsData.tsx
+++ src/Data/locationsData.tsx
@@ -10,6 +10,8 @@ interface SlideContent {
   gymName: string;
   gymStreet: string;
   gymPerks: string;
+  gymCord: number[];
+  popUp: string;
 }
 
 export const slides: SlideContent[] = [
@@ -19,12 +21,16 @@ export const slides: SlideContent[] = [
     gymStreet: "Savanorių pr. 168, Kaunas",
     gymPerks:
       "FREE PARKING, GROUP TRAININGS, PERSONAL TRAINING, BASKETBALL MACHINE",
+    gymCord: [54.9073607, 23.9264831],
+    popUp: "test11"
   },
   {
     locationPic: Vienuolio,
     gymName: "Vienuolis",
     gymStreet: "Vienuolio g. 4, Vilnius",
     gymPerks: "GROUP TRAININGS, SAUNA, PERSONAL TRAINING, SPACIOUS CARDIO AREA",
+    gymCord: [55.2461399, 24.7904016],
+    popUp: "test11"
   },
   {
     locationPic: Pilaite,
@@ -32,12 +38,16 @@ export const slides: SlideContent[] = [
     gymStreet: "Vydūno g. 2, Vilnius",
     gymPerks:
       "FREE PARKING, PERSONAL TRAINING, BASKETBALL MACHINE, FOOTBALL TABLE",
+    gymCord: [54.9073607, 23.9264831],
+    popUp: "test11"
   },
   {
     locationPic: Europa,
     gymName: "Europa",
     gymStreet: "Konstitucijos pr. 7A, Vilnius",
     gymPerks: "GROUP TRAININGS, PERSONAL TRAINING, SAUNA, FOOTBALL TABLE",
+    gymCord: [54.9073607, 23.926483441],
+    popUp: "test11"
   },
   {
     locationPic: Banginis,
@@ -45,6 +55,8 @@ export const slides: SlideContent[] = [
     gymStreet: "P. Lukšio g. 34, Vilnius",
     gymPerks:
       "FREE PARKING, PERSONAL TRAINING, GROUP TRAININGS, FOOTBALL TABLE",
+    gymCord: [54.9073607, 23.9264841],
+    popUp: "test11"
   },
   {
     locationPic: Antakalnis,
@@ -52,5 +64,7 @@ export const slides: SlideContent[] = [
     gymStreet: "Antakalnio g. 37, Vilnius",
     gymPerks:
       "FREE PARKING, PERSONAL TRAINING, FOOTBALL TABLE, SPACIOUS CARDIO AREA",
+    gymCord: [54.9073607, 23.9264841],
+    popUp: "test11"
   },
 ];
