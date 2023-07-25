5337fbdfe22648bf7995f09d8961ada461f39abb
Aldas <80693733+aldask@users.noreply.github.com>
2023-07-25 21:58:35 +0300

    A bit of styling done

--- src/Components/Comparison/Comparison.tsx
+++ src/Components/Comparison/Comparison.tsx
@@ -1,5 +1,5 @@
+import React, { useState } from "react";
 import { slides, SlideContent } from "../../Data/locationsData";
-import { useState } from "react";
 
 function Comparison() {
   const [selectedGymAddress, setSelectedGymAddress] = useState<SlideContent>(
@@ -14,6 +14,7 @@ function Comparison() {
       setSelectedGymAddress(selectedGym);
     }
   };
+
   const planTitle = (index: number): string => {
     switch (index) {
       case 0:
@@ -32,9 +33,9 @@ function Comparison() {
       <div className="container">
         <div className="comparison-box">
           <div className="comparison-box__top">
-            <h2>Select your favourite gym</h2>
+            <h2>Choose one of our gyms below!</h2>
             <div className="comparison-box__top--selection">
-              <label>Select your gym</label>
+              <label>Select your gym: {" "}</label>
               <select name="gyms" onChange={handleSelectedGym}>
                 {slides.map((gym) => (
                   <option key={gym.gymName} value={gym.gymName}>
@@ -44,24 +45,22 @@ function Comparison() {
               </select>
             </div>
             <div className="comparison-box__top--display">
-              <div className="comparison-box__top--display--plan">
-                <h2>{selectedGymAddress.gymName}</h2>
-                <p>{selectedGymAddress.gymStreet}</p>
-                <div>
-                  {selectedGymAddress.plans.map((plan, index) => (
-                    <div key={index}>
-                      <h3>{planTitle(index)}</h3>
-                      <p>Price: {plan.Price}€/month</p>
-                      <ul>
-                        {plan.features.map((feature, featureIndex) => (
-                          <li key={featureIndex}>
-                            {feature.name} - {feature.benefits ? "+" : "-"}
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                  ))}
-                </div>
+              <h2>{selectedGymAddress.gymName}</h2>
+              <p>{selectedGymAddress.gymStreet}</p>
+              <div className="comparison-box__top--display--plans">
+                {selectedGymAddress.plans.map((plan, index) => (
+                  <div key={index} className="comparison-box__top--display--plans--plan">
+                    <h3>{planTitle(index)}</h3>
+                    <p>Price: {plan.Price}€/month</p>
+                    <ul>
+                      {plan.features.map((feature, featureIndex) => (
+                        <li key={featureIndex}>
+                          {feature.name} - {feature.benefits ? "+" : "-"}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ))}
               </div>
             </div>
           </div>
