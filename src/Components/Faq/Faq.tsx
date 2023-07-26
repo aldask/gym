fa96098cc41e3ece386eebd1cefc9e4f3085c24f
Aldas <80693733+aldask@users.noreply.github.com>
2023-07-26 21:11:18 +0300

    Faq init

---
+++ src/Components/Faq/Faq.tsx
@@ -0,0 +1,10 @@
+function Faq() {
+  return (
+    <section className="faq">
+      <div className="container">
+        <div className="faq-box">faq here</div>
+      </div>
+    </section>
+  );
+}
+export default Faq;
