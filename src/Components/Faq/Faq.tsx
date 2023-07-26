62bfd8ec38ef4f9f8640ec6f21622eaf5f064772
Aldas <80693733+aldask@users.noreply.github.com>
2023-07-26 21:50:50 +0300

    Now need to show question answer

--- src/Components/Faq/Faq.tsx
+++ src/Components/Faq/Faq.tsx
@@ -1,10 +1,41 @@
+import React, { useState } from "react";
+import { faqContent } from "../../Data/faq";
+
 function Faq() {
+  const [selectedQuestion, setSelectedQuestion] = useState("");
+
+  const handleQuestion = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const selectedQuestion = faqContent.find(
+      (question) => question.ask === e.currentTarget.value
+    );
+
+    if (selectedQuestion) {
+      setSelectedQuestion(selectedQuestion.answer);
+    } else {
+      setSelectedQuestion("");
+    }
+  };
+
   return (
     <section className="faq">
       <div className="container">
-        <div className="faq-box">faq here</div>
+        <div className="faq-box">
+          <div className="faq-box__question-box">
+            {faqContent.map((question) => (
+              <div key={question.ask}>
+                <button
+                  className="faq-box__question-box--question"
+                  onClick={handleQuestion}
+                >
+                  {question.ask}
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </section>
   );
 }
+
 export default Faq;
---
+++ src/Data/faq.tsx
@@ -0,0 +1,19 @@
+interface faqContent {
+  ask: string;
+  answer: string;
+}
+
+export const faqContent: faqContent[] = [
+  {
+    ask: "question 1",
+    answer: "answer 1",
+  },
+  {
+    ask: "question 2",
+    answer: "answer 2",
+  },
+  {
+    ask: "question 3",
+    answer: "answer 3",
+  },
+];
