1287a6007cdcf3e4b903b22bca60af2bfeb1a607
Aldas <80693733+aldask@users.noreply.github.com>
2023-07-27 21:38:26 +0300

    Faq done, i guess...

--- src/Components/Faq/Faq.tsx
+++ src/Components/Faq/Faq.tsx
@@ -15,13 +15,17 @@ function Faq() {
     <section className="faq">
       <div className="container">
         <div className="faq-box">
+          <div className="faq-box__header">
+            <h2>WE’VE GOT ANSWERS</h2>
+            <p>If you can’t find what you’re looking for, please <span><a href = "mailto: hello@irongym.com">contact us</a></span>.</p>
+          </div>
           <div className="faq-box__question-box">
             {faqContent.map((question) => (
-              <div key={question.ask} className="faq-box__question-box--question">
-                <button
-                  onClick={handleQuestion}
-                  value={question.ask}
-                >
+              <div
+                key={question.ask}
+                className="faq-box__question-box--question"
+              >
+                <button onClick={handleQuestion} value={question.ask}>
                   {question.ask}
                 </button>
                 {selectedQuestion === question.ask && (
