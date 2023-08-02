a9ad70f88033108f5d400de5048b76978e9c2308
Aldas <80693733+aldask@users.noreply.github.com>
2023-08-02 18:53:59 +0300

    About init

---
+++ src/Components/LetsConnect/LetsConnect.tsx
@@ -0,0 +1,10 @@
+function LetsConnect() {
+  return (
+    <section className="connect">
+      <div className="container">
+        <h1>LET’S CONNECT</h1>
+      </div>
+    </section>
+  );
+}
+export default LetsConnect;
