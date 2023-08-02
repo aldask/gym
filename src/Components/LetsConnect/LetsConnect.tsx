7e042b5a908de452d2f77a17ca626681d93b94b1
Aldas <80693733+aldask@users.noreply.github.com>
2023-08-02 19:29:01 +0300

    Layout done. I think...

--- src/Components/LetsConnect/LetsConnect.tsx
+++ src/Components/LetsConnect/LetsConnect.tsx
@@ -2,7 +2,35 @@ function LetsConnect() {
   return (
     <section className="connect">
       <div className="container">
-        <h1>LET’S CONNECT</h1>
+        <div className="connect-box">
+          <div className="connect-box__top">
+            <h1>LET’S CONNECT</h1>
+            <p>Feel free to contact us & ask your questions personally.</p>
+          </div>
+          <div className="connect-box__bottom">
+            <div className="connect-box__bottom--side1">
+              <div className="connect-box__bottom--side1--email">
+                <div>logo</div>
+                <div>
+                  <a href="mailto:hello@irongym.com">hello@irongym.com</a>
+                </div>
+              </div>
+              <div className="connect-box__bottom--side1--phone">
+                <div>logo</div>
+                <div>
+                  <a href="tel:86123">86123</a>
+                </div>
+              </div>
+              <div className="connect-box__bottom--side1--company">
+                <div>logo</div>
+                <div>
+                  <p>stuff about company</p>
+                </div>
+              </div>
+            </div>
+            <div className="connect-box__bottom--side2">contact form here</div>
+          </div>
+        </div>
       </div>
     </section>
   );
