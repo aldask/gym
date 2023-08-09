ec6ded6f1715adaa3b4e61e750748683bdf23ebc
Aldas Kasputis <80693733+aldask@users.noreply.github.com>
2023-08-09 18:30:28 +0300

    The coaches layout done. Now need to make seperate file for coaches and map it

--- src/Components/Trainers/TrainersList.tsx
+++ src/Components/Trainers/TrainersList.tsx
@@ -1,4 +1,7 @@
 import coach from "../../Images/Trainers/coach.jpg";
+import IG from "../../Images/Trainers/IG.png";
+import Phone from "../../Images/Trainers/Phone.png";
+import Mail from "../../Images/Trainers/Mail.png";
 
 function TrainersList() {
   return (
@@ -31,7 +34,28 @@ function TrainersList() {
                   <h2>Eddie Murphy</h2>
                 </div>
                 <div className="trainers-box__bottom__trainers--trainer--details">
-                  coach details here
+                  <div className="trainers-box__bottom__trainers--trainer--details--info">
+                    <h2>Description</h2>
+                    <p>
+                      coach is good dudecoach is good dudecoach is good
+                      dudecoach is good dude
+                    </p>
+                  </div>
+                  <div className="trainers-box__bottom__trainers--trainer--details--contacts">
+                    <h2>Contacts</h2>
+                    <div className="trainers-box__bottom__trainers--trainer--details--contacts--ig">
+                      <img src={IG} alt="ig" />
+                      <a href="instagram.com/username">eddie.murphy</a>
+                    </div>
+                    <div className="trainers-box__bottom__trainers--trainer--details--contacts--phone">
+                      <img src={Phone} alt="phone" />
+                      <a href="tel:+3706112233">+3706112233</a>
+                    </div>
+                    <div className="trainers-box__bottom__trainers--trainer--details--contacts--email">
+                      <img src={Mail} alt="mail" />
+                      <a href="mailto:azuolas@gmail.com">azuolas@gmail.com</a>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
