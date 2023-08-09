368e8035ae1aecdaa007c546f4a8bc24e66d1106
Aldas Kasputis <80693733+aldask@users.noreply.github.com>
2023-08-09 14:19:47 +0300

    A bit of coaches styling

--- src/Components/Trainers/TrainersList.tsx
+++ src/Components/Trainers/TrainersList.tsx
@@ -1,3 +1,5 @@
+import coach from "../../Images/Trainers/coach.jpg";
+
 function TrainersList() {
   return (
     <section className="trainers">
@@ -6,15 +8,33 @@ function TrainersList() {
           src="https://www.lemongym.lt/wp-content/uploads/2023/02/1440x520_trainers_5-1.png"
           alt=""
         />
-        <h1>MEET OUR TEAM</h1>
+        <h1>MEET OUR COACHES</h1>
       </div>
       <div className="container">
         <div className="trainers-box">
           <div className="trainers-box__top">
-            <h1>top</h1>
+            <h1>FOLLOW ALONG EXPERT INSTRUCTORS</h1>
+            <p>
+              In the sports clubs you will find a wide range of group training.
+              From light physical activity for the elderly to intense functional
+              training for advanced club visitors
+            </p>
           </div>
           <div className="trainers-box__bottom">
-            <h1>bottom</h1>
+            <h1>ALL TRAINERS</h1>
+            <div className="trainers-box__bottom__trainers">
+              <div className="trainers-box__bottom__trainers--trainer">
+                <div className="trainers-box__bottom__trainers--trainer--img">
+                  <img src={coach} alt="coach" />
+                </div>
+                <div className="trainers-box__bottom__trainers--trainer--name">
+                  <h2>Eddie Murphy</h2>
+                </div>
+                <div className="trainers-box__bottom__trainers--trainer--details">
+                  coach details here
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
