9a062eded3e3608a35cd10f9d762351c574d0754
Aldas Kasputis <80693733+aldask@users.noreply.github.com>
2023-08-09 13:21:17 +0300

    Staff update1

--- src/Components/Trainers/TrainersList.tsx
+++ src/Components/Trainers/TrainersList.tsx
@@ -1,8 +1,22 @@
 function TrainersList() {
   return (
     <section className="trainers">
+      <div className="trainers-hero-img">
+        <img
+          src="https://www.lemongym.lt/wp-content/uploads/2023/02/1440x520_trainers_5-1.png"
+          alt=""
+        />
+        <h1>MEET OUR TEAM</h1>
+      </div>
       <div className="container">
-        <div className="trainers-box">trainers here</div>
+        <div className="trainers-box">
+          <div className="trainers-box__top">
+            <h1>top</h1>
+          </div>
+          <div className="trainers-box__bottom">
+            <h1>bottom</h1>
+          </div>
+        </div>
       </div>
     </section>
   );
