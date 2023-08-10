1e978835596785b850746bc1ab7c258fd487daec
Aldas Kasputis <80693733+aldask@users.noreply.github.com>
2023-08-10 23:40:02 +0300

    Enough for today

--- src/Components/WhatCanDo/WhatCanDo.tsx
+++ src/Components/WhatCanDo/WhatCanDo.tsx
@@ -1,4 +1,5 @@
 import What from "../../Images/Trial/What.jpg";
+import Benefit from "../../Images/Misc/benefit_logo.png";
 
 function WhatCanDo() {
   return (
@@ -17,15 +18,19 @@ function WhatCanDo() {
           <div className="what-can-do__box__image">
             <img src={What} alt="what-you-can-do"></img>
             <h2 className="what-can-do__box__image--box1">
+              <img src={Benefit} alt="logo" />
               COME TOGETHER WITH A FRIEND
             </h2>
             <h2 className="what-can-do__box__image--box2">
+              <img src={Benefit} alt="logo" />
               ATTEND ANY GROUP TRAINING
             </h2>
             <h2 className="what-can-do__box__image--box3">
+              <img src={Benefit} alt="logo" />
               VISIT ANY CLUB 1 TIME
             </h2>
             <h2 className="what-can-do__box__image--box4">
+              <img src={Benefit} alt="logo" />
               EXERCISE IN THE GYM
             </h2>
           </div>
