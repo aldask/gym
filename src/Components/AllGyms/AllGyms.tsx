bfaf7c28ff494d0badb370dc11938a2735e4b8a4
Aldas Kasputis <80693733+aldask@users.noreply.github.com>
2023-08-11 22:02:38 +0300

    Still didnt decided if i'll keep the animation on hovering

--- src/Components/AllGyms/AllGyms.tsx
+++ src/Components/AllGyms/AllGyms.tsx
@@ -1,12 +1,58 @@
-function AllGyms () {
-    return (
-        <section className="all-gyms">
-            <div className="container">
-                <div className="all-gyms-box">
-                    boxes
+import React from "react";
+import { slides } from "../../Data/locationsData";
+
+function AllGyms() {
+  return (
+    <section className="all-gyms">
+      <div className="all-gyms-hero-img">
+        <img
+          src="https://www.lemongym.lt/wp-content/uploads/2023/02/club-bg_1440x520_2-1.jpg"
+          alt=""
+        />
+        <h1>EXPLORE OUR GYMS</h1>
+      </div>
+      <div className="container">
+        <div className="all-gyms-box">
+          <div className="all-gyms-box__top">
+            <h1>FIND GYM THAT SUITS YOU BEST</h1>
+            <p>
+              A variety of locations in Lithuania. Iron Gym clubs are situated
+              in easy-to-find public places with 24/7 access.
+            </p>
+          </div>
+          <div className="all-gyms-box__bottom">
+            <h1>OUR GYMS</h1>
+            <div className="all-gyms-box__bottom__gyms">
+              {slides.map((gym, index) => (
+                <div key={index} className="all-gyms-box__bottom__gyms--gym">
+                  <div className="all-gyms-box__bottom__gyms--gym--details">
+                    <div className="all-gyms-box__bottom__gyms--gym--details--info">
+                      <h2>Description</h2>
+                      <p>{gym.description}</p>
+                    </div>
+                    <div className="all-gyms-box__bottom__gyms--gym--details--benefits">
+                      <h2>Benefits</h2>
+                      <p>{gym.gymPerks}</p>
+                    </div>
+                    <div className="all-gyms-box__bottom__gyms--gym--details--prices">
+                      <h2>Plan prices starting from: <span>{gym.plans[0].Price}</span></h2>
+                    </div>
+                  </div>
+                  <div className="all-gyms-box__bottom__gyms--gym--img">
+                    <img src={gym.locationPic} alt={gym.gymName} />
+                  </div>
+                  <div className="all-gyms-box__bottom__gyms--gym--name">
+                    <h2>{gym.gymName}</h2>
+                    <p>{gym.gymStreet}</p>
+                  </div>
                 </div>
+              ))}
             </div>
-        </section>
-    )
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
+
 export default AllGyms;
--- src/Data/locationsData.tsx
+++ src/Data/locationsData.tsx
@@ -9,6 +9,7 @@ export interface SlideContent {
   locationPic: string;
   gymName: string;
   gymStreet: string;
+  description: string;
   gymPerks: string;
   gymCord: number[];
   popUp: string;
@@ -26,6 +27,7 @@ export const slides: SlideContent[] = [
     locationPic: Savanoriai,
     gymName: "Savanoriai",
     gymStreet: "Savanorių pr. 168, Kaunas",
+    description: "Savanoriai Lemon Gym – a club close to your work and home! The club is young and youthful, and here you can find an exercise space for calm stretches, a wide functional area, and a group training room for real sports parties! You won’t feel crowded in the main hall either – it’s spacious, and there are enough exercise machines for everyone. For active and fun breaks, you will find a basketball machine and the comfiest massage chairs ever… Everything to help you achieve your goals!",
     gymPerks:
       "FREE PARKING, GROUP TRAININGS, PERSONAL TRAINING, BASKETBALL MACHINE",
     gymCord: [54.9077288679767, 23.926749497639175],
@@ -75,6 +77,7 @@ export const slides: SlideContent[] = [
     locationPic: Vienuolio,
     gymName: "Vienuolis",
     gymStreet: "Vienuolio g. 4, Vilnius",
+    description: "The new Vienuolis LEMON GYM opens its doors on August 1! In this club, you will find the latest sports equipment, exercise machines, spacious functional and cardio areas with a view of the old town of Vilnius. We invite you to workout together with LEMON GYM community – in 2 group training halls!",
     gymPerks: "GROUP TRAININGS, SAUNA, PERSONAL TRAINING, SPACIOUS CARDIO AREA",
     gymCord: [54.68854730571488, 25.27646161112141],
     popUp: "<h2>Vienuolis</h2><p>Vienuolio g. 4, Vilnius</p>",
@@ -123,6 +126,7 @@ export const slides: SlideContent[] = [
     locationPic: Pilaite,
     gymName: "Pilaitė",
     gymStreet: "Vydūno g. 2, Vilnius",
+    description: "Lemon Gym Pilaite – your nearest home club! Here you will find an exercise space for calm stretching, as well as a functional area. You won’t feel crowded in the main workout hall either – it’s spacious, and there is enough exercise equipment for everyone. You will exercise with modern equipment, tested and loved by professionals. Everything to achieve your goals!",
     gymPerks:
       "FREE PARKING, PERSONAL TRAINING, BASKETBALL MACHINE, FOOTBALL TABLE",
     gymCord: [54.70736374382466, 25.189616339958025],
@@ -172,6 +176,7 @@ export const slides: SlideContent[] = [
     locationPic: Europa,
     gymName: "Europa",
     gymStreet: "Konstitucijos pr. 7A, Vilnius",
+    description: "You will find the newly renovated club in the PC Europa building, on the 3rd floor! Here you will find an exercise space for calm stretching, a wide functional area, and a group training room for real workout parties! You won’t feel crowded in the main exercise area either – it’s spacious, and there is enough exercise equipment for everyone. Don’t forget to capture your sports achievements next to the famous golden mirror in the functional area;)",
     gymPerks: "GROUP TRAININGS, PERSONAL TRAINING, SAUNA, FOOTBALL TABLE",
     gymCord: [54.69619096586617, 25.27625653070827],
     popUp: "<h2>Europa</h2><p>Konstitucijos pr. 7A, Vilnius</p>",
@@ -220,6 +225,7 @@ export const slides: SlideContent[] = [
     locationPic: Banginis,
     gymName: "Banginis",
     gymStreet: "P. Lukšio g. 34, Vilnius",
+    description: "Banginis Lemon Gym – a club close to your work and home. Here you will find an exercise space for calm stretching, a wide functional area, and a group training room for real workout parties! Go through the second floor – here you will find a spacious cardio training area and a BOOTY ZONE! You won’t feel crowded in the main hall either – it’s spacious, and there are enough exercise machines for everyone.",
     gymPerks:
       "FREE PARKING, PERSONAL TRAINING, GROUP TRAININGS, FOOTBALL TABLE",
     gymCord: [54.71034513961146, 25.29922939447378],
@@ -269,6 +275,7 @@ export const slides: SlideContent[] = [
     locationPic: Antakalnis,
     gymName: "Antakalnis",
     gymStreet: "Antakalnio g. 37, Vilnius",
+    description: "Antakalnis Lemon Gym – a club close to your home! This is a club of like-minded people, where everyone knows each other! Here you will find an exercise space for quiet stretching, as well as a functional area. When you come to the hall, you won’t feel crowded either – it’s spacious, and there are enough exercise machines for everyone. You will be working out with modern equipment, tested and loved by professionals.",
     gymPerks:
       "FREE PARKING, PERSONAL TRAINING, FOOTBALL TABLE, SPACIOUS CARDIO AREA",
     gymCord: [54.70386195755908, 25.310660926464948],
