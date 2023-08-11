63110a8498f146e60db34da76d9a1bf9ed6938e7
Aldas Kasputis <80693733+aldask@users.noreply.github.com>
2023-08-11 19:11:02 +0300

    Gyms init

---
+++ src/Components/AllGyms/AllGyms.tsx
@@ -0,0 +1,12 @@
+function AllGyms () {
+    return (
+        <section className="all-gyms">
+            <div className="container">
+                <div className="all-gyms-box">
+                    boxes
+                </div>
+            </div>
+        </section>
+    )
+}
+export default AllGyms;
